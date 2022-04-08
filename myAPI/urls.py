767efc353c935ef6a556d9defb4f7f6aca03ae9d
John Emmanuel Aquino <johneman220@gmail.com>
2022-04-08 21:47:40 +0800

    work done third commit

--- myAPI/urls.py
+++ myAPI/urls.py
@@ -1,8 +1,9 @@
 from django.urls import include, path
 from rest_framework import routers
+from . import views
 
 router = routers.DefaultRouter()
-router.register('vehicls', views.vehiclviewset)
+router.register('vehicls', views.vehiclsviewset)
 
 urlpatterns = [
     path('', include(router.urls)),
